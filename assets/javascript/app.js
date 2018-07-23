656149cf7fdcb1b85c760f3e07313d22387d1bda
Justin Riddle <justinriddle@Justins-iMac.local>
2018-07-23 14:42:13 -0700

    updated javascript and fixed errors

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,61 +1,102 @@
+var count = 60;
 
+var correctCount = 0;
+var wrongCount = 0;
+var unansweredCount = 0;
 
- $("#startClock").click( function(){
-     setInterval(function() {
-      counter--;
-       if (counter >= 0) {
-           span = document.getElementById("count");
-           span.innerHTML = counter;
-       }
-       if (counter === 0) {
-          alert('sorry, out of time');
-           clearInterval(counter);
-         }
-      }, 1000);
- });
+$(document).ready(function () {
 
- function clearAnswers(){
-    $("img").each(function(){
-      $(this).remove();
-    })
-  }
-  
-  function markIncorrect(el){
-    var newText = text("<p>wrong!</p>");
+  $("#mid_game_container").hide();
+  $("#end_container").hide();
 
-    el.append(newText);
-  }
-  
-  function markCorrect(el){
-    var img = new Image();
-    img.src = 'http://www.littletherese.com/tick.jpg';
-    el.append(img);
-  }
-  
-  $("form").on("submit", function(e){
-    e.preventDefault();
-    clearAnswers();
-  
-    $questions = $(".question");
-    $questions.each(function(){
-      var answer = $(this).find("input:checked"),
-          key = answer.attr("name"),
-          val = answer.attr("value");
-  
-      if(answer.length === 0){
-        markIncorrect($(this).find("p"));
-      } else if (answers[key] !== val){
-        markIncorrect($(this).find("p"));
-      } else {
-        markCorrect(answer.parent());
-      }
-    });
+  $("#startbutton").on("click", function () {
+
+    $("#start_container").hide();
+    $("#mid_game_container").show();
+
+    startCountdown();
+    return;
+
   });
-  
-  var answers = {
-    "one": "b",
-    "two": "d",
-    "three": "b",
-    "four": "a",
-    "five": "d"
+
+  function countdown() {
+    count--;
+    $('#timer_number').html(count + " Seconds");
+    $("#done_button").on("click", function () {
+      count = 0;
+      return;
+    });
+
+    if (count == -1) {
+      timeUp();
+      $("#mid_game_container").hide();
+    }
   }
+
+  function startCountdown() {
+    setInterval(countdown, 1000);
+  }
+  function timeUp() {
+    var Q1 = $('input:radio[name="q1"]:checked').val();
+    var Q2 = $('input:radio[name="q2"]:checked').val();
+    var Q3 = $('input:radio[name="q3"]:checked').val();
+    var Q4 = $('input:radio[name="q4"]:checked').val();
+    var Q5 = $('input:radio[name="q5"]:checked').val();
+
+    if (Q1 == undefined) {
+      unansweredCount++;
+    }
+    else if (Q1 == "Ringo") {
+      correctCount++;
+    }
+    else {
+      wrongCount++;
+    }
+
+    if (Q2 == undefined) {
+      unansweredCount++;
+    }
+    else if (Q2 == "The Ed Sullivan Show") {
+      correctCount++;
+    }
+    else {
+      wrongCount++;
+    }
+
+    if (Q3 == undefined) {
+      unansweredCount++;
+    }
+    else if (Q3 == "All My Loving") {
+      correctCount++;
+    }
+    else {
+      wrongCount++;
+    }
+
+    if (Q4 == undefined) {
+      unansweredCount++;
+    }
+    else if (Q4 == "$4") {
+      correctCount++;
+    }
+    else {
+      wrongCount++;
+    }
+
+    if (Q5 == undefined) {
+      unansweredCount++;
+    }
+    else if (Q5 == "Pete Best") {
+      correctCount++;
+    }
+    else {
+      wrongCount++;
+    }
+
+    $('#correct_answers').html(correctCount);
+    $('#wrong_answers').html(wrongCount);
+    $('#unanswered').html(unansweredCount);
+    $("#end_container").show();
+
+  }
+});
